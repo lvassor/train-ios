5c5e0a48edfda39cfd725c6c2dae9718878569e0
Luke Vassor <luke.vassor96@gmail.com>
2025-11-14 15:28:45 +0100

    Remove web app - moved to separate repository

--- api/index.js
+++
@@ -1,5 +0,0 @@
-// api/index.js - Vercel serverless function entry point
-const app = require('../backend/server.js');
-
-// Export the Express app as a serverless function
-module.exports = app;
--- public/README.md
+++
@@ -1,1 +0,0 @@
-# Static assets served by Express
